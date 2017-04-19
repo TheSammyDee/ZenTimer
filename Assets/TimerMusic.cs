32a9c9b060541e354b1af0a1e0fd6be4d0a3197e
Samantha Derochie <Samantha Derochie>
2017-04-18 20:58:06 -0700

    Created TimesLogger class for displaying lists of times, started TimerMusic class

---
+++ Assets/TimerMusic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerMusic : MonoBehaviour {
+
+    private AudioSource music;
+    private bool isFadingIn = false;
+    private bool isFadingOut = false;
+    private float fadeStep = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+        music = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isFadingIn && music.volume < 1) {
+            music.volume += fadeStep * Time.deltaTime;
+        }
+	}
+}
---
+++ Assets/TimesLogger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimesLogger : MonoBehaviour {
+
+    private Text logText;
+
+	// Use this for initialization
+	void Start () {
+        logText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public string AddTimeText(float time) {
+        string oldText = logText.text;
+        logText.text = oldText + ConvertTimeToString(time) + "\n";
+        return logText.text;
+    }
+
+    public string AddDatedTimeText(float time) {
+        string oldText = logText.text;
+        string newText = System.DateTime.Now.ToString("MMM dd") + " " + ConvertTimeToString(time) + "\n";
+        logText.text = oldText + newText;
+        return logText.text;
+    }
+
+    public string ConvertTimeToString(float time) {
+        int minutes = (int)(time / 60);
+        int seconds = (int)(time % 60);
+        string timeString = minutes.ToString() + ":" + seconds.ToString("00");
+        return timeString;
+    }
+
+    public void ClearText() {
+        logText.text = "";
+    }
+}
